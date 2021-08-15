c5bd73a839b84ea12a0c4be9b2bddc38fff01375
Irving Henriquez <henriquezirv@gmail.com>
2021-08-15 17:40:39 -0400

    completed login form and styling, moved images dir to src

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/signup"));
 const Profile = lazy(() => import("./pages/profile"));
 const NotFound = lazy(() => import("./pages/not-found"));
-console.log(Login);
 export default function App() {
   return (
     <Router>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,64 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+import logo from "../images/logo.png";
+import iphone from "../images/iphone-with-profile.jpg";
 export default function Login() {
-  return <p>Login</p>;
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(emailAddress);
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || emailAddress === "";
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  }, []);
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen ">
+      <div className="flex w-3/5">
+        <img src={iphone} alt="iphone with Instagram App" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src={logo} alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+          <form method="POST">
+            <input
+              aria-label="Enter your email address"
+              className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+              type="text"
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${
+                isInvalid && " cursor-not-allowed opacity-50"
+              }`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border">
+          <p className="text-sm">
+            Don't have an account?
+            <Link to={ROUTES.SIGN_UP} className="font-bold">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
