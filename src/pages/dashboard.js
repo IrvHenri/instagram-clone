5dfa9ccd80e1dc2094385053097e75a8ae4eb173
Irving Henriquez <henriquezirv@gmail.com>
2021-08-16 12:52:55 -0400

    set up dashboard skeleton with native components

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Header from "../components/header";
+import Timeline from "../components/timeline";
+import Sidebar from "../components/sidebar";
 
 export default function Dashboard() {
-  return <p>Hello from dashboard!</p>;
+  useEffect(() => {
+    document.title = "Instagram";
+  });
+  return (
+    <div className="bg-gray-100">
+      <Header />
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+        <Timeline />
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
