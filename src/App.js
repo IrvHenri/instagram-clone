ceea0e9b622ed2a8a6d46559456309373f0f6a69
Irving Henriquez <henriquezirv@gmail.com>
2021-08-16 13:10:02 -0400

    finished not found page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
           <Route path={ROUTES.PROFILE} component={Profile} />
           <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
-          <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+          <Route component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,5 +1,25 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
 export default function NotFound() {
-  return <div></div>;
+  useEffect(() => {
+    document.title = "Page Not Found";
+  });
+  return (
+    <div className="bg-gray-200">
+      <div className="mx-auto max-w-screen-lg">
+        <h1 className="text-center text-2xl pt-6 mb-8">
+          Sorry, this page isn't available.
+        </h1>
+
+        <p className="text-center">
+          The link you followed may be broken, or the page may have been
+          removed.
+          <Link to={ROUTES.DASHBOARD} className="font-bold">
+            Go back to Instagram
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
