593d718c9533950a8d6b887874734ed07ef35e10
Irving Henriquez <henriquezirv@gmail.com>
2021-08-17 23:25:16 -0400

    created use-auth-listener hook connected to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import * as ROUTES from "./constants/routes";
+import UserContext from "./context/user";
+import useAuthListener from "./hook/use-auth-listener";
 
 const Dashboard = lazy(() => import("./pages/dashboard"));
 const Login = lazy(() => import("./pages/login"));
@@ -9,17 +12,20 @@ const SignUp = lazy(() => import("./pages/signup"));
 const Profile = lazy(() => import("./pages/profile"));
 const NotFound = lazy(() => import("./pages/not-found"));
 export default function App() {
+  const { user } = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-          <Route path={ROUTES.PROFILE} component={Profile} />
-          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider user={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.PROFILE} component={Profile} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
