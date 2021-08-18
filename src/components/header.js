5dc28a7fbfc9dad1ab2765489a0709f361c25876
Irving Henriquez <henriquezirv@gmail.com>
2021-08-17 23:12:24 -0400

    added user context skeleton and added svgs to header

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,11 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../constants/routes";
 import FirebaseContext from "../context/firebase";
-import logo from "../images/logo.png";
 export default function Header() {
   const { firebase } = useContext(FirebaseContext);
-  const user = null;
+  const user = {
+    displayName: "raphael",
+  };
   return (
     <header className="h-16 bg-white border-b mb-8">
       <div className="container mx-auto max-width-lg h-full">
@@ -13,7 +14,11 @@ export default function Header() {
           <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
             <h1>
               <Link to={ROUTES.DASHBOARD} alt="Dashbaord">
-                <img src={logo} alt="instagram logo" className="mt-2 w-6/12" />
+                <img
+                  src="/images/logo.png"
+                  alt="instagram logo"
+                  className="mt-2 w-6/12"
+                />
               </Link>
             </h1>
           </div>
@@ -21,7 +26,20 @@ export default function Header() {
             {user ? (
               <>
                 <Link to={ROUTES.DASHBOARD} arial-label="Home">
-                  <p>Dashboard</p>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 mr-6 text-black-light cursor-pointer"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                    />
+                  </svg>
                 </Link>
                 <button
                   type="button"
@@ -33,7 +51,20 @@ export default function Header() {
                     }
                   }}
                 >
-                  Sign Out
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 mr-6 text-black-light cursor-pointer"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    />
+                  </svg>
                 </button>
                 <div className="flex items-center cursor-pointer">
                   <Link to={`/p/${user.displayName}`}>
