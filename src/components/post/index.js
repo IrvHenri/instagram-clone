f82a2c689b464889ccfae8dd3938f3b28b21ac73
Irving Henriquez <henriquezirv@gmail.com>
2021-08-19 21:36:36 -0400

    completed actions component with conditional renderd svgs with firestore likes update

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "./header";
 import Image from "./image";
 import Actions from "./actions";
@@ -6,9 +6,17 @@ import Footer from "./footer";
 import Comments from "./comments";
 
 export default function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
   return (
     <div className="rounded col-span-4 border bg-white mb-16">
       <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
       <Footer username={content.username} caption={content.caption} />
     </div>
   );
