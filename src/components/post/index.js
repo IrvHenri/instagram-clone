e66711c8975a00a9c163267b3ddd7d3be058f495
Irving Henriquez <henriquezirv@gmail.com>
2021-08-19 20:57:40 -0400

    finished footer component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -9,6 +9,7 @@ export default function Post({ content }) {
   return (
     <div className="rounded col-span-4 border bg-white mb-16">
       <Image src={content.imageSrc} caption={content.caption} />
+      <Footer username={content.username} caption={content.caption} />
     </div>
   );
 }
