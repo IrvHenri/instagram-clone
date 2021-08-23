462fd01c06ed1cfa10c230d805fb6f3b94c0daa0
Irving Henriquez <henriquezirv@gmail.com>
2021-08-23 15:49:26 -0400

    finished add-comment strcuture with handlesubmit firebase call

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import FirebaseContext from "../../context/firebase";
+import UserContext from "../../context/user";
 
-const AddComment = ({}) => {
-  return null;
+const AddComment = ({ docId, comments, setComments }) => {
+  const [comment, setComment] = useState("");
+  const { firebase, FieldValue } = useContext(FirebaseContext);
+  const {
+    user: { displayName },
+  } = useContext(UserContext);
+
+  const handleSubmitComment = (event) => {
+    event.preventDefault();
+
+    setComments((prev) => [{ displayName, comment }, ...prev]);
+    setComment("");
+
+    return firebase
+      .firestore()
+      .collection("photos")
+      .doc(docId)
+      .update({
+        comments: FieldValue.arrayUnion({ displayName, comment }),
+      });
+  };
+
+  return (
+    <div className="border-t border-gray-500">
+      <p>I will be a form!</p>
+    </div>
+  );
 };
 
 export default AddComment;
--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -6,24 +6,32 @@ import AddComment from "./add-comment";
 const Comments = ({ docId, comments: allComments, posted, commentInput }) => {
   const [comments, setComments] = useState(allComments);
   return (
-    <div className="p-4 pt-1 pb-4">
-      {comments.length >= 3 && (
-        <p className="text-sm text-gray-500 mb-1 cursor-pointer">
-          View all {comments.length} comments
+    <>
+      <div className="p-4 pt-1 pb-4">
+        {comments.length >= 3 && (
+          <p className="text-sm text-gray-500 mb-1 cursor-pointer">
+            View all {comments.length} comments
+          </p>
+        )}
+        {comments.slice(0, 3).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span>{item.comment}</span>
+          </p>
+        ))}
+        <p className="text-gray uppercase text-xs mt-2">
+          {formatDistance(posted, new Date())} ago
         </p>
-      )}
-      {comments.slice(0, 3).map((item) => (
-        <p key={`${item.comment}-${item.displayName}`} className="mb-1">
-          <Link to={`/p/${item.displayName}`}>
-            <span className="mr-1 font-bold">{item.displayName}</span>
-          </Link>
-          <span>{item.comment}</span>
-        </p>
-      ))}
-      <p className="text-gray uppercase text-xs mt-2">
-        {formatDistance(posted, new Date())} ago
-      </p>
-    </div>
+      </div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
+    </>
   );
 };
 
