115fc84892b37598693a744a9d15395a56bd8ada
Irving Henriquez <henriquezirv@gmail.com>
2021-08-23 16:49:05 -0400

    finished user sidebar component

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import useUser from "../../hooks/use-user";
-
+import User from "./user";
 export default function Sidebar() {
   const { user: { docId, userId, username, following, fullName } = {} } =
     useUser();
 
-  return <p>Sidebar</p>;
+  return (
+    <div className="p-4">
+      <User username={username} fullName={fullName} />
+    </div>
+  );
 }
