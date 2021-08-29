f1a680b1f2eb775bb4ae619b22cc00b2bb40e333
Irving Henriquez <henriquezirv@gmail.com>
2021-08-28 23:07:46 -0400

    finished profile header component styling

--- src/components/proflile/header.js
+++ src/components/proflile/header.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from "react";
+import useUser from "../../hooks/use-user";
 
 export default function Header(props) {
   const { photosCount, followerCount, setFollowerCount, profile, username } =
     props;
-  return <p>I am header</p>;
+  const { user } = useUser();
+  const [isFollowingProfile, setIsFollowingProfile] = useState(false);
+  const activeBtnFollow = user.username && user.username !== username;
+  return (
+    <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+      <div className="container flex justify-center">
+        <img
+          className="rounded-full h-40 w-40 flex"
+          alt={`${username} profile picture`}
+          src={`/images/avatars/${username}.jpg`}
+        />
+      </div>
+      <div className="flex items-center justify-center flex-col col-span-2">
+        <div className="container flex items-center">
+          <p className="text-2xl mr-4">{username}</p>
+          {activeBtnFollowState && (
+            <button
+              className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8"
+              type="button"
+              onClick={() => console.log("I am a button")}
+            >
+              {isFollowingProfile ? "Unfollow" : "Follow"}
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
